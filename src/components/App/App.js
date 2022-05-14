189d93a4f640429441a05cd676e4e177c3cb0b33
sani2608 <sanikumarsahani@gmail.com>
2022-05-15 03:51:24 +0530

    initial project setup

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <span className="header">🎬 Entertainment Hub 🎥</span>
+    </div>
+  );
+}
+
+export default App;
