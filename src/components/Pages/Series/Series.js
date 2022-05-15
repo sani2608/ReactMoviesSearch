6250f6652ef6739deeb6d34ed3507322189e87e6
sani2608 <sanikumarsahani@gmail.com>
2022-05-15 21:14:26 +0530

    series comp

---
+++ src/components/Pages/Series/Series.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Series.css'
+
+const Series = () => {
+    return (
+        <div>
+            <span className="pagetitle">Series</span>
+        </div>
+    )
+}
+
+export default Series
