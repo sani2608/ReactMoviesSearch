904d22f7ace70ad5a7e12344c6b1d2d89503aec6
sani2608 <sanikumarsahani@gmail.com>
2022-05-15 21:13:05 +0530

    app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,31 @@
 import React from "react";
+import Header from "../Header/Header";
 import "./App.css";
+import SimpleBottomNavigation from "../MainNav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container } from "@material-ui/core";
+import Trending from "../Pages/Trending/Trending";
+import Movies from "../Pages/Movies/Movies";
+import Series from "../Pages/Series/Series";
+import Search from "../Pages/Search/Search";
 
 function App() {
   return (
-    <div className="App">
-      <span className="header">🎬 Entertainment Hub 🎥</span>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <Container>
+          <Routes>
+            <Route path="/" exact element={<Trending />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/series" element={<Series />} />
+            <Route path="/search" element={<Search />} />
+          </Routes>
+        </Container>
+      </div>
+
+      <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
