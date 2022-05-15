62675e7ca4f4a01f2a503ad7d6ccaf03207f995a
sani2608 <sanikumarsahani@gmail.com>
2022-05-15 21:14:11 +0530

    movie comp

---
+++ src/components/Pages/Movies/Movies.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Movies.css'
+
+const Movies = () => {
+  return (
+    <div>
+      <span className="pagetitle">
+        Movies
+      </span>
+
+    </div>
+  )
+}
+
+export default Movies
