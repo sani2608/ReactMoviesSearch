b2366bb04483dacbb3625e53ec0cd9bfddb7a023
sani2608 <sanikumar.sahani@gmail.com>
2022-05-17 15:11:40 +0530

    added pagination in movies and trending tab

--- src/components/Pages/Movies/Movies.js
+++ src/components/Pages/Movies/Movies.js
@@ -1,15 +1,50 @@
-import React from 'react'
-import './Movies.css'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import CustomPagination from "../../Pagination/CustomPagination";
+import SingleContent from "../../SingleContent/SingleContent";
+import "./Movies.css";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Movies = () => {
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numberOfPages, setnumberOfPages] = useState(1);
+
+  const fetchMovies = async () => {
+    // const {data} = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=${page}&with_genres=${genreForURL}`);
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=${page}`
+    );
+    setContent(data.results);
+    setnumberOfPages(data.total_pages);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [page]);
+
   return (
     <div>
-      <span className="pagetitle">
-        Movies
-      </span>
-
+      <span className="pagetitle">Movies</span>
+      <div className="trending">
+        {content &&
+          content.map((item) => (
+            <SingleContent
+              key={item.id}
+              id={item.id}
+              poster={item.poster_path}
+              title={item.title || item.name}
+              date={item.release_date || item.first_air_date}
+              mdeia_type={item.media_type}
+              vote_average={item.vote_average}
+            />
+          ))}
+      </div>
+      {numberOfPages > 1 && (
+        <CustomPagination setPage={setPage} numberOfPages={numberOfPages} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Movies
+export default Movies;
--- src/components/Pages/Trending/Trending.js
+++ src/components/Pages/Trending/Trending.js
@@ -1,45 +1,48 @@
-import React, { useEffect } from 'react';
-import './Trending.css';
-import axios from 'axios';
-import SingleContent from '../../SingleContent/SingleContent';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import CustomPagination from "../../Pagination/CustomPagination";
+import SingleContent from "../../SingleContent/SingleContent";
+import "./Trending.css";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Trending = () => {
-  const [content, setContent] = React.useState([]);
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
 
   const fetchTrending = async () => {
-    console.log('fetching data');
-    const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`);
+    console.log("fetching data");
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}&page=${page}`
+    );
     setContent(data.results);
     // console.log(data.results);
   };
 
   useEffect(() => {
     fetchTrending();
-  }, []);
-
+  }, [page]);
 
   return (
     <div>
       <span className="pagetitle">Trending Today</span>
-        <div className="trending">
-          {
-            content && content.map((item) => (
-              <SingleContent
-                key={item.id}
-                id={item.id}
-                poster={item.poster_path}
-                title={item.title || item.name}
-                date={item.release_date || item.first_air_date}
-                mdeia_type={item.media_type}
-                vote_average={item.vote_average}
-              />))
-          }
-        </div>
-      
+      <div className="trending">
+        {content &&
+          content.map((item) => (
+            <SingleContent
+              key={item.id}
+              id={item.id}
+              poster={item.poster_path}
+              title={item.title || item.name}
+              date={item.release_date || item.first_air_date}
+              mdeia_type={item.media_type}
+              vote_average={item.vote_average}
+            />
+          ))}
+      </div>
+      <CustomPagination setPage={setPage} />
     </div>
-  )
-}
+  );
+};
 
-export default Trending
+export default Trending;
